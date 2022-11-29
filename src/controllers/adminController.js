91f5c254a39da121f267da50b6dae5c875f36163
fersantisi <fer.santisi@hotmail.com>
2022-11-29 15:16:28 -0300

    PUT & DELETE CRUDs

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -20,24 +20,79 @@ const adminController = {
     store: (req, res) => {
         let newProduct = {
             "id": products[products.length-1].id + 1,
+            "brand": req.body.brand,
             "name": req.body.name,
+            "category": req.body.category,
             "price": req.body.price,
-            "stock": req.body.stock,
+            "discount": req.body.discount,
+            "image": req.body.image,
             "description": req.body.description
-        }
+        };
 
         products.push(newProduct);
 
 		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, '\t'));
 
-        res.render('./admin/adminIndex', {products})
-    }
-    ,
+        res.render('./admin/adminIndex', {products});
+    },
 
     users: (req, res) => {
         console.log('Entre al registro de perfiles');
         res.render('./admin/userAdmin');
-    }   
-}
+    },
+
+    manage: (req, res) => {
+        let product =  products.find( product => product.id == req.params.id)
+        console.log('Entre a administrador de producto');
+        res.render('./admin/manageProduct', {product});
+    },
+
+    edit: (req,res) => {
+        let product = products.find((product) => {
+			return product.id == req.params.id;
+		});
+		res.render('./admin/editProduct', {product})
+    },
+
+    update: (req,res) => {
+        let productToEdit = products.find((product) => {
+			return product.id == req.params.id;
+		});
+
+		let editedProduct = {
+			"id": productToEdit.id,
+            "brand": req.body.brand,
+            "name": req.body.name,
+            "category": req.body.category,
+            "price": req.body.price,
+            "discount": req.body.discount,
+            "image": req.body.image,
+            "description": req.body.description
+		};
+
+		let updatedJSON = products.map((product) => {
+			if(editedProduct.id == product.id){
+				return product = editedProduct;
+			}
+			return product;
+		});
+
+		fs.writeFileSync(productsFilePath, JSON.stringify(updatedJSON, null, '\t'));
+
+		res.render('./admin/manageProduct', {product: editedProduct});
+    },
+
+    destroy: (req,res) => {
+        let productToDelete = products.find((product) => {
+			return product.id == req.params.id;
+		});
+
+		let removed = products.splice(products.indexOf(productToDelete), 1);
+
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, '\t'));
+
+		res.redirect('/');
+    },
+};
 
 module.exports = adminController;
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -4,9 +4,16 @@ const adminController = require("../controllers/adminController")
 
 router.get('/', adminController.admin)
 
+router.get('/manageProduct/:id', adminController.manage);
+router.get('/manageProduct/edit/:id', adminController.edit);
+router.put('/manageProduct/edit/:id', adminController.update);
+router.delete('/manageProduct/delete/:id', adminController.destroy);
+
 router.get("/createProduct", adminController.create);
-router.get("/userAdmin", adminController.users);
 router.post("/createProduct", adminController.store);
 
+
+
+router.get("/userAdmin", adminController.users);
 
 module.exports = router;
