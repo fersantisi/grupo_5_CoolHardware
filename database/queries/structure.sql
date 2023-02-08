964f5e4dccfc6e9e336262883e3cf19a0970f9e0
ignacioOreiro <ignaciooreiro3@outlook.es>
2023-02-08 18:36:14 -0300

    queries structure

--- database/queries/structure.sql
+++ database/queries/structure.sql
@@ -1,20 +1,20 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
+SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS,
+  UNIQUE_CHECKS = 0;
+SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS,
+  FOREIGN_KEY_CHECKS = 0;
+SET @OLD_SQL_MODE = @@SQL_MODE,
+  SQL_MODE = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
 -- -----------------------------------------------------
 -- Schema cool_hardware
 -- -----------------------------------------------------
-
 -- -----------------------------------------------------
 -- Schema cool_hardware
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `cool_hardware` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `cool_hardware` ;
-
+CREATE SCHEMA IF NOT EXISTS `cool_hardware` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `cool_hardware`;
 -- -----------------------------------------------------
 -- Table `cool_hardware`.`Brand`
 -- -----------------------------------------------------
@@ -24,12 +24,8 @@ CREATE TABLE IF NOT EXISTS `cool_hardware`.`Brand` (
   `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` TIMESTAMP NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_general_ci;
-
-
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci;
 -- -----------------------------------------------------
 -- Table `cool_hardware`.`Category`
 -- -----------------------------------------------------
@@ -40,12 +36,8 @@ CREATE TABLE IF NOT EXISTS `cool_hardware`.`Category` (
   `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` TIMESTAMP NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_general_ci;
-
-
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci;
 -- -----------------------------------------------------
 -- Table `cool_hardware`.`Product`
 -- -----------------------------------------------------
@@ -54,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `cool_hardware`.`Product` (
   `name` VARCHAR(255) NOT NULL,
   `description` TEXT NULL DEFAULT NULL,
   `stock` INT NOT NULL,
-  `price` DECIMAL(10,2) NOT NULL,
+  `price` DECIMAL(10, 2) NOT NULL,
   `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `discount` INT NULL DEFAULT NULL,
@@ -65,21 +57,9 @@ CREATE TABLE IF NOT EXISTS `cool_hardware`.`Product` (
   PRIMARY KEY (`id`, `category_id`, `brand_id`),
   INDEX `fk_Product_Category1_idx` (`category_id` ASC),
   INDEX `fk_Product_Brand1_idx` (`brand_id` ASC),
-  CONSTRAINT `fk_Product_Category1`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `cool_hardware`.`Category` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Product_Brand1`
-    FOREIGN KEY (`brand_id`)
-    REFERENCES `cool_hardware`.`Brand` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_general_ci;
-
-
+  CONSTRAINT `fk_Product_Category1` FOREIGN KEY (`category_id`) REFERENCES `cool_hardware`.`Category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Product_Brand1` FOREIGN KEY (`brand_id`) REFERENCES `cool_hardware`.`Brand` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci;
 -- -----------------------------------------------------
 -- Table `cool_hardware`.`User`
 -- -----------------------------------------------------
@@ -94,13 +74,108 @@ CREATE TABLE IF NOT EXISTS `cool_hardware`.`User` (
   `image` VARCHAR(255) NULL DEFAULT 'Default.jpg',
   `admin` TINYINT(1) NOT NULL DEFAULT '0',
   `is_active` TINYINT(1) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 13
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_general_ci;
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 13 DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci;
+SET SQL_MODE = @OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+INSERT INTO product (
+    `name`,
+    `description`,
+    `stock`,
+    `price`,
+    `discount`,
+    `image`,
+    `is_active`,
+    `category_id`,
+    `brand_id`
+  )
+VALUES (
+    'TUF Gaming FX506L',
+    'Procesador: Intel Core i5',
+    10,
+    380000,
+    50,
+    'notebook.jpg',
+    1,
+    1,
+    1
+  );
+INSERT INTO product (
+    `name`,
+    `description`,
+    `stock`,
+    `price`,
+    `discount`,
+    `image`,
+    `is_active`,
+    `category_id`,
+    `brand_id`
+  )
+VALUES (
+    'Aorus FI25F 25"',
+    '"Pantalla: 25"',
+    15,
+    320000,
+    30,
+    'monitor.jpg',
+    1,
+    2,
+    2
+  );
+INSERT INTO product (
+    `name`,
+    `description`,
+    `stock`,
+    `price`,
+    `discount`,
+    `image`,
+    `is_active`,
+    `category_id`,
+    `brand_id`
+  )
+VALUES (
+    'HS35 Stereo',
+    'Micrófono: Si',
+    4,
+    320000,
+    0,
+    'auricular.jpg',
+    1,
+    3,
+    3
+  );
+INSERT INTO category (`name`)
+VALUES ('Notebook');
+INSERT INTO category (`name`)
+VALUES ('Monitor');
+INSERT INTO category (`name`)
+VALUES ('Auricular');
+INSERT INTO brand (`name`)
+VALUES ('Asus');
+INSERT INTO brand (`name`)
+VALUES ('Gigabyte');
+INSERT INTO brand (`name`)
+VALUES ('Corsair');
+INSERT INTO user (
+    `first_name`,
+    `last_name`,
+    `nickname`,
+    `email`,
+    `password`,
+    `image`,
+    `admin`,
+    `is_active`
+  )
+VALUES (
+    'admin',
+    'admin',
+    'admin',
+    'admin@admin.com',
+    '$2b$10$lMGjoQKbZ9zQM5P7I9NqZepgWS6b4HTDQkiMuu2S.Cpop.rq/rFEC',
+    'Admin.jpg',
+    1,
+    1
+  );
