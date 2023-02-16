de522619d065ef90775f3864a195fa875bc7a919
ignacioOreiro <ignaciooreiro3@outlook.es>
2023-02-16 16:49:52 -0300

    validator 80%

--- public/js/registerValidator.js
+++ public/js/registerValidator.js
@@ -1,58 +1,107 @@
+const form = document.getElementById("form");
 const firstname = document.getElementById("firstname");
 const lastname = document.getElementById("lastname");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const pass = document.getElementById("pass");
 const passConfirm = document.getElementById("passConfirm");
-const form = document.getElementById("form");
+const errorP = document.querySelector("#errormsg")
+
+const inputs = [form,firstname,lastname,username,email,pass,passConfirm,]
+
+const error = [];
+
+inputs.forEach((input, i)=> {
+    input.addEventListener("input", (e)=>{
+        const div = e.target.parentElement;
+        const errorP = div.lastElementChild
+        
+        if (firstname.value === '' || firstname.value === null) {
+            if(!error.includes(i)){
+                error.push(i);
+            }
+            errorFirst.innerText = "Error en nombre";
+        }else{
+            if(error.lastIndexOf(i) !== -1){
+                error = error.filter(error => error !== i)
+            }
+            errorFirst.innerText = "";
+        }
+        if (lastname.value === '' || lastname.value === null) {
+            if(!error.includes(i)){
+                error.push(i);
+            }
+            errorP.innerText = "Error en apelid"
+        }else{
+            if(error.lastIndexOf(i) !== -1){
+                error = error.filter(error => error !== i)
+            }
+            errorP.innerText = '';
+        }
+        if (username.value === '' || username.value === null) {
+            if(!error.includes(i)){
+                error.push(i);
+            }
+            errorP.innerText = "Error en username"
+        }else{
+            if(error.lastIndexOf(i) !== -1){
+                error = error.filter(error => error !== i)
+            }
+            errorP.innerText = '';
+        }
+        if (email.value === '' || email.value === null) {
+            if(!error.includes(i)){
+                error.push(i);
+            }
+            errorP.innerText = "Error en email"
+        }else{
+            if(error.lastIndexOf(i) !== -1){
+                error = error.filter(error => error !== i)
+            }
+            errorP.innerText = '';
+        }
+        if (pass.value.length <= 8 || pass.value === '' || pass.value === null) {
+            if(!error.includes(i)){
+                error.push(i);
+            }
+            errorP.innerText = "Error en pass"
+        }else{
+            if(error.lastIndexOf(i) !== -1){
+                error = error.filter(error => error !== i)
+            }
+            errorP.innerText = '';
+        }
+        if (pass.value.length >= 20) {
+            if(!error.includes(i)){
+                error.push(i);
+            }
+            errorP.innerText = "Error en pass leng"
+        }else{
+            if(error.lastIndexOf(i) !== -1){
+                error = error.filter(error => error !== i)
+            }
+            errorP.innerText = '';
+        } 
+        if (!passConfirm.value === pass) {
+            if(!error.includes(i)){
+                error.push(i);
+            }
+            errorP.innerText = "Error en passC"
+        }else{
+            if(error.lastIndexOf(i) !== -1){
+                error = error.filter(error => error !== i)
+            }
+            errorP.innerText = '';
+        } 
+        if(error){
+            console.log("hay errores");
+            document.getElementById('habilitar').disabled = "disabled"
+        }else{
+            console.log("no hay errores");
+            document.getElementById('habilitar').disabled = "";
+        }
+    });
+});
 
 
-const regEx = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-const message = []
-form.addEventListener("submit", (e) => {
-
-    if (firstname.value === '' || firstname.value === null) {
-        message.push("El nombre es necesario")
-    } else {
-        message.pop()
-    }
-    if (lastname.value === '' || lastname.value === null) {
-        message.push("El apellido es necesario")
-    } else {
-        message.pop()
-    }
-    if (username.value === '' || username.value === null) {
-        message.push("El nombre de usuario es necesario")
-    } else {
-        message.pop()
-    }
-    if (regEx.test(email.value)) {
-        message.push('El email es invalido')
-    } else {
-        message.pop()
-    }
-    if (pass.value.length <= 8 || pass.value === '' || pass.value === null) {
-        message.push('La contraseña tiene que ser mayor a 8 caracteres')
-    } else {
-        message.pop()
-    }
-    if (pass.value.length >= 20) {
-        message.push('La contraseña tiene que ser menor a 20 caracteres')
-    } else {
-        message.pop()
-    }
-    if (!passConfirm.value === pass) {
-        message.push('Las contraseña deben coincidir')
-    } else {
-        message.pop()
-    }
-
-    if (message.length > 0) {
-        e.preventDefault();
-        let errorElement = document.querySelector("#errorElement");
-        message.forEach(error => {
-            errorElement.innerHTML += `<p>${error}</p>`
-        })
-    }
-})
 
