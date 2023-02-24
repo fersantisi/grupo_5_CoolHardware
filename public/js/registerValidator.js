60f12479bd489b4308894cfaa5d78336f6a51b2b
fersantisi <fer.santisi@hotmail.com>
2023-02-23 21:21:12 -0300

    front register validation

--- public/js/registerValidator.js
+++ public/js/registerValidator.js
@@ -5,107 +5,91 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const pass = document.getElementById("pass");
 const passConfirm = document.getElementById("passConfirm");
-const errorP = document.querySelector("#errormsg")
 
-const inputs = Array.from(document.querySelectorAll('.formulario input'))
+const errFirstname = document.getElementById("errFirstname");
+const errLastname = document.getElementById("errLastname");
+const errUsername = document.getElementById("errUsername");
+const errEmail = document.getElementById("errEmail");
+const errPass = document.getElementById("errPass");
+const errPassConfirm = document.getElementById("errPassConfirm");
 
-const error = [];
 
-inputs.forEach((input, i)=> {
-    input.addEventListener("input", (e)=>{
-        const div = e.target.parentElement;
-        const errorP = div.lastElementChild
-        
-        if (e.target.firstname.value === '' || e.target.firstname.value === null) {
-            if(!error.includes(i)){
-                error.push(i);
-            }
-            errorP.innerText = "Error en nombre";
-        }else{
-            if(error.lastIndexOf(i) !== -1){
-                error = error.filter(error => error !== i)
-            }
-            errorP.innerText = "";
-        }
-        if (e.target.lastname.value === '' || e.target.lastname.value === null) {
-            if(!error.includes(i)){
-                error.push(i);
-            }
-            errorP.innerText = "Error en apelid"
-        }else{
-            if(error.lastIndexOf(i) !== -1){
-                error = error.filter(error => error !== i)
-            }
-            errorP.innerText = '';
-        }
-        if (e.target.username.value === '' || e.target.username.value === null) {
-            if(!error.includes(i)){
-                error.push(i);
-            }
-            errorP.innerText = "Error en username"
-        }else{
-            if(error.lastIndexOf(i) !== -1){
-                error = error.filter(error => error !== i)
-            }
-            errorP.innerText = '';
-        }
-        if (e.target.email.value === '' || e.target.email.value === null) {
-            if(!error.includes(i)){
-                error.push(i);
-            }
-            errorP.innerText = "Error en email"
-        }else{
-            if(error.lastIndexOf(i) !== -1){
-                error = error.filter(error => error !== i)
-            }
-            errorP.innerText = '';
-        }
-        if (e.target.pass.value.length <= 8 || e.target.pass.value === '' || e.target.pass.value === null) {
-            if(!error.includes(i)){
-                error.push(i);
-            }
-            errorP.innerText = "Error en pass"
-        }else{
-            if(error.lastIndexOf(i) !== -1){
-                error = error.filter(error => error !== i)
-            }
-            errorP.innerText = '';
-        }
-        if (e.target.pass.value.length >= 20) {
-            if(!error.includes(i)){
-                error.push(i);
-            }
-            errorP.innerText = "Error en pass leng"
-        }else{
-            if(error.lastIndexOf(i) !== -1){
-                error = error.filter(error => error !== i)
-            }
-            errorP.innerText = '';
-        } 
-        if (!e.target.passConfirm.value === pass) {
-            if(!error.includes(i)){
-                error.push(i);
-            }
-            errorP.innerText = "Error en passC"
-        }else{
-            if(error.lastIndexOf(i) !== -1){
-                error = error.filter(error => error !== i)
-            }
-            errorP.innerText = '';
-        } 
-        if (error.length > 0) {
-            document.getElementById('habilitar').disabled = "disabled"
-        }else{
-            document.getElementById('habilitar').disabled = ""
-        }
-
-        inputs.forEach(input => {
-            if(input.value === ''){
-                document.getElementById('habilitar').disabled = "disabled"
-            }
-        });
-    });
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("form").addEventListener('submit', validarFormulario);
 });
 
+function validarFormulario(evento) {
+    evento.preventDefault();
+    let counter = 0;
+    /* firstname */
+    if (firstname.value == "") {
+        errFirstname.style.display = "block";
+        errFirstname.innerHTML = "Enter your first name F";
+        firstname.focus();
+        counter++;
+    } else {
+        errFirstname.style.display = "none";
+    }
+    /* lastname */
+    if (lastname.value === "") {
+        errLastname.style.display = "block";
+        errLastname.innerHTML = "Enter your last name F";
+        lastname.focus();
+        counter++;
+    } else {
+        errLastname.style.display = "none";
+    }
+    /* username */
+    if (username.value === "") {
+        errUsername.style.display = "block";
+        errUsername.innerHTML = "Enter your user name F";
+        username.focus();
+        counter++;
+    } else {
+        errUsername.style.display = "none";
+    }
+    /* Email */
+    const charEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)+\.\S+/.test(email.value);
+    if (email.value === "") {
+        errEmail.style.display = "block";
+        errEmail.innerHTML = "Enter your email F";
+        email.focus();
+        counter++;
+    } else if (!charEmail) {
+        errEmail.style.display = "block";
+        errEmail.innerHTML = "Invalid format F";
+        email.focus();
+        counter++;
+    } else {
+        errEmail.style.display = "none";
+    }
+    /* pass */
+    console.log(pass.value.length);
+    if (pass.value === "") {
+        errPass.style.display = "block";
+        errPass.innerHTML = "Enter your password F";
+        pass.focus();
+        counter++;
+    } else if (pass.value.length < 8) {
+        errPass.style.display = "block";
+        errPass.innerHTML = "La contraseña debe tener al menos 8 caracteres F";
+        pass.focus();
+        counter++;
+    } else {
+        errPass.style.display = "none";
+    }
+    /* passconfirm */
+    if (passConfirm.value !== pass.value || passConfirm.value === '') {
+        errPassConfirm.style.display = "block";
+        errPassConfirm.innerHTML = "Tus contraseñas no coinciden F";
+        passConfirm.focus();
+        counter++;
+    } else {
+        errPassConfirm.style.display = "none";
+    }
+    console.log(counter);
+    if (counter == 0) {
+        evento.target.submit();
+    }
 
-
+}
